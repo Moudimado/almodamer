7169c35c1b1767034fb3a00d9ecf6da21947a9e7
kamal200947 <58574229+kamal200947@users.noreply.github.com>
2019-12-08 22:21:37 +0200

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -464,7 +464,7 @@ def start(target, counter, delay, ch, cc):
 
 
 def update():
-    stuff_to_update = ['bomber.py', '.version']
+    stuff_to_update = ['almodamer.py', '.version']
     for fl in stuff_to_update:
         dat = urllib.request.urlopen(
             "https://raw.githubusercontent.com/kamal200947/almodamer/master/" + fl).read()
